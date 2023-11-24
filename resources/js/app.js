5919700d73fe0db3557dd766acde8187b616e10e
ZwillingIII <kirill.knyazev.20@mail.ru>
2023-11-24 17:33:04 +0300

    add fetch in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,25 @@
-import './bootstrap';
+// import './bootstrap';
 
-function fetch($data) {
-  return true;
+// ! TODO: более 1000 запросов за раз
+function fetch(action, method, form) {
+  return fetch(action, {
+    method: method,
+    // body: new FormData(form),
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 }
 
-// отправка формы изменения поста
-const postEditForm = document.querySelector('#edit-post');
-postEditForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const data = {
-    "action": e.srcElement.action,
-    "method": e.target.method,
-  };
-
-  console.log(e);
-
-  // return false;
+document.addEventListener('DOMContentLoaded', function() {
+  // отправка формы изменения поста
+  const postEditForm = document.querySelector('#edit-post');
+  postEditForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+  
+    fetch(e.target.action, e.target.method, postEditForm);
+  });
 });
--- resources/views/components/formPutPost.blade.php
+++ resources/views/components/formPutPost.blade.php
@@ -1,4 +1,4 @@
-<form id="edit-post" data-code="{{ $code }}" action="{{ route("posts.edit", $code) }}" method="GET">
+<form id="edit-post" data-code="{{ $code }}" action="{{ route("posts.edit", $code, false) }}" method="GET">
   <input type="text" name="name" placeholder="test" value="">
   <button type="submit">Send</button>
 </form>
